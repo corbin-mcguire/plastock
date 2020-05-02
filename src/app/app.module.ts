23dc2b3763c0183cf5388b24078b692903c90970
Corbin <mcguire.corbin@gmail.com>
2020-05-02 01:03:05 -0400

    Header component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlHeaderComponent } from './lib/pl-header/pl-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlHeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/lib/pl-header/pl-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pl-header',
+  templateUrl: './pl-header.component.html',
+  styleUrls: ['./pl-header.component.scss']
+})
+export class PlHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
