87d7a113b1ee32573244578011639640c609a5a7
Corbin McGuire <mcguire.corbin@gmail.com>
2020-05-14 21:57:58 -0400

    Moved components into the components folder

--- src/app/lib/components/filament-grid/filament-grid.component.ts
+++ src/app/lib/components/filament-grid/filament-grid.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FilamentService } from '../services/filament.service';
-import { Filament } from '../types/filament.type';
+import { FilamentService } from '../../services/filament.service';
+import { Filament } from '../../types/filament.type';
 
 @Component({
   selector: 'app-filament-grid',
