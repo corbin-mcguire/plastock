793eb97b551361347c0c328feee39bfa77e4901a
Corbin McGuire <mcguire.corbin@gmail.com>
2020-05-18 23:34:51 -0400

    Wired it all up

--- src/app/lib/services/filament.service.ts
+++ src/app/lib/services/filament.service.ts
@@ -1,19 +1,36 @@
 import { Injectable } from '@angular/core';
 import { Filament } from '../types/filament.model';
+import { Subject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FilamentService {
   private filaments: Filament[] = [];
+  filamentDetailSubject: Subject<Filament> = new Subject();
+  isFilamentDetailShown: boolean;
 
-  constructor() {}
+  constructor() {
+    this.isFilamentDetailShown = false;
+  }
 
   addFilament(newFilament: Filament) {
     this.filaments.push(newFilament);
+    // TODO: POST request
   }
 
   getFilaments(): Filament[] {
     return this.filaments;
+    // TODO: GET request
+  }
+
+  getFilamentDetailObservable(): Observable<Filament> {
+    return this.filamentDetailSubject.asObservable();
+  }
+
+  removeFilament(filament: Filament): void {
+    this.filaments.splice(this.filaments.indexOf(filament));
+    this.isFilamentDetailShown = false;
+    // TODO: DELETE request
   }
 }
